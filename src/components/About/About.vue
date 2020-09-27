6fec552c246bcfb7da2c6b79d4122b3974f3429b
fredgoum <alfredgoumou@gmail.com>
2020-09-27 21:14:01 +0200

    fix footer

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -6,7 +6,7 @@
         <span class="footer-subtitle" style="">Rejoignez </span>
         <strong>Emirates | The list</strong>
       </div>
-      <div class="footer-subtitle">
+      <div id="footer-subtitle">
         Vol + hôtel négociés jusqu'à -70%
       </div>
       <img class="black-red" width="250" height="3" src="@/assets/dash.png" alt="dash">
